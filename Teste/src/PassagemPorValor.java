0d954e3b788b1a02768b061de8667c31e21a1468
maced <maced@DESKTOP-23DGUBU>
2019-03-06 19:09:26 -0300

    Passagem por valor

---
+++ Teste/src/PassagemPorValor.java
@@ -0,0 +1,16 @@
+
+public class PassagemPorValor {
+
+	public static int dobra(int num) {
+		return num*2;
+
+	}
+
+	public static void main (String[] args) {
+		int numero = 2112;
+		System.out.println("O valor de número é: " + numero);
+		System.out.println("Dobrando seu valor.");
+		dobra(numero);
+		System.out.println("Agora o valor de número é: " + dobra(numero));
+	}
+}
